71e5dbabfe68275b0252151879834e1c715bf29e
zeeshandar <azfar.imtiaz@live.com>
2019-02-25 02:00:23 +0100

    Initial commit

--- gendoc.py
+++ gendoc.py
@@ -6,10 +6,102 @@ import pandas as pd
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfTransformer
 
-# gendoc.py -- Don't forget to put a reasonable amount code comments
-# in so that we better understand what you're doing when we grade!
+import re
+from nltk.corpus import stopwords
+from collections import Counter, defaultdict
 
-# add whatever additional imports you may need here
+# this function explores all directories within the provided path
+# for each sub-folder found, all text files are read into a list of dicts
+# each dict here contains filename, filepath and text of file
+def load_text_from_files(path_to_files):
+    dirs = os.listdir(path_to_files)
+    all_files_data = []
+    for sub_dir in dirs:
+        if sub_dir.startswith(".DS_Store"):
+            continue
+        full_dir_path = os.path.join(path_to_files, sub_dir)
+        files = os.listdir(full_dir_path)
+        
+        # need to remove this later!
+        # files = files[:30]
+        
+        for filename in files:
+            # if file is not .txt format, ignore it
+            if not filename.lower().endswith(".txt"):
+                continue
+
+            full_file_path = os.path.join(full_dir_path, filename)
+            text = ''
+            with open(full_file_path, 'r') as rfile:
+                text = " ".join(rfile.readlines())
+            data = {
+                'filename': filename,
+                'filepath': full_dir_path,
+                'text': text
+            }
+            all_files_data.append(data)
+            
+    return all_files_data
+
+
+# this function strips away all punctuation marks, strips leading and trailing whitespaces and converts to lowercase
+def preprocess_text(text):
+    # The regex to remove punct marks here needs to be more sophisticated
+    # Punctuation marks in the documents are prepended and appended by a space. "U . S .", "department ' s"
+    text = text.lower()
+    text = " ".join([a for a in text.split() if a not in stopwords.words('english')])
+    # regex to convert numbers like 1563 . 1 to 1563.1 or 100 , 000 to 100,000
+    text = re.sub(r'(?<=\d)\s([^\w\s]+)(\s|$)(?=\d+)', r'\1', text)
+    # remove all punctuation marks that do not occur between numbers, such as "My name, indeed, is Azfar"
+    text = re.sub(r'(?<!\d)[^ \w](?!\d)', '', text)
+    # replace multiple spaces with a single space
+    text = re.sub(r'\s+', ' ', text)
+    # rg_punct = r'[^A-Za-z\d ]'
+    # text = re.sub(rg_punct, '', text)
+    text = text.strip()
+    return text
+
+
+# preprocess text of all files
+def preprocess_files_text(all_files_data):
+    for index, data in enumerate(all_files_data):
+        cleaned_text = preprocess_text(data['text'])
+        all_files_data[index]['cleaned_text'] = cleaned_text
+    return all_files_data
+
+
+# this function gets word counts of all words in the corpus
+def get_all_word_counts(all_files_data):
+    word_counts = defaultdict(lambda: 0)
+    for index, data in enumerate(all_files_data):
+        cleaned_text = all_files_data[index]['cleaned_text']
+        tokens = cleaned_text.split()
+        for token in tokens:
+            word_counts[token] += 1
+
+    return word_counts
+
+
+# this function adds word counts in all_files_data per document
+# if relevant_words is not None, this means that we get counts for only those words in documents which are in relevant_words
+# if relevant_words is None, we get counts of all words in documents
+def create_word_counts_per_doc(all_files_data, relevant_words):
+    for index, data in enumerate(all_files_data):
+        tokens = data['cleaned_text'].split()
+        if relevant_words is not None:
+            for token in tokens:
+                try:
+                    relevant_words[token]
+                    all_files_data[index][token] = tokens.count(token)
+                except KeyError:
+                    # this means this word is not in relevant_words; ignore
+                    continue
+        else:
+            for token in tokens:
+                all_files_data[index][token] = tokens.count(token)
+
+    return all_files_data
+
 
 parser = argparse.ArgumentParser(description="Generate term-document matrix.")
 parser.add_argument("-T", "--tfidf", action="store_true", help="Apply tf-idf to the matrix.")
@@ -26,20 +118,98 @@ parser.add_argument("outputfile", type=str,
 
 args = parser.parse_args()
 
+# add a check to ensure that args.basedims is not greater than or equal to args.svddims, since that isn't possible
+if args.basedims and args.svddims:
+    if args.basedims >= args.svddims:
+        print("The number of dimensions for SVD should be less than the number of base dimensions!")
+        exit(1)
+
+# ensure that the output file specified is csv
+if not args.outputfile.lower().endswith("csv"):
+    print("Please specify an output file with .csv extension!")
+    exit(1)
+
 print("Loading data from directory {}.".format(args.foldername))
+all_files_data = load_text_from_files(args.foldername)
+
+print("Preprocessing text...")
+all_files_data = preprocess_files_text(all_files_data)
+
+relevant_words = None
 
 if not args.basedims:
     print("Using full vocabulary.")
 else:
     print("Using only top {} terms by raw count.".format(args.basedims))
+    # get counts of all words in all documents - this returns a dictionary
+    word_counts_all = get_all_word_counts(all_files_data)
+    # create a Counter object of all word counts
+    word_counts_counter = Counter(word_counts_all)
+    # get M most common words from corpus by using most_common method of Counter object
+    relevant_words = dict(word_counts_counter.most_common(args.basedims))
 
-if args.tfidf:
-    print("Applying tf-idf to raw counts.")
+# get word counts of all documents, on document level
+all_files_data = create_word_counts_per_doc(all_files_data, relevant_words)
+
+# create a dataframe object
+df = pd.DataFrame(all_files_data)
+
+# fill all null values with 0
+df.fillna(0, inplace=True)
+
+# get all duplicates in the dataframe, dropping columns like filepath, filename, text and cleaned_text which might hinder duplicate detection
+duplicates = df.duplicated(subset=[a for a in df.columns if a not in ['filepath', 'filename', 'text', 'cleaned_text']])
+for index in range(len(duplicates)):
+    if bool(duplicates[index]) is True:
+        print("%s/%s will be dropped!" % (df['filepath'].iloc[index], df['filename'].iloc[index]))
+
+# drop all duplicates
+df_deduplicated = df.drop_duplicates(subset=[a for a in df.columns if a not in ['filepath', 'filename', 'text', 'cleaned_text']])
+
+# get filenames and filepaths - these will be used for multi-index later
+filenames = df_deduplicated['filename']
+filepaths = df_deduplicated['filepath']
+
+# drop the filename and filepath columns; these will be later used for a multi-index
+df_word_counts = df_deduplicated.drop(['filepath', 'filename', 'text', 'cleaned_text'], axis=1, inplace=False)
 
 if args.svddims:
-    print("Truncating matrix to {} dimensions via singular value decomposition.".format(args.svddims))
+    if args.svddims >= len(df_word_counts.columns):
+        print("The number of dimensions for SVD should be less than the total number of features!")
+        exit(1)
+word_scores = df_word_counts
 
-# THERE ARE SOME ERROR CONDITIONS YOU MAY HAVE TO HANDLE WITH CONTRADICTORY
-# PARAMETERS.
+# if tfidf is specified, apply it to transform scores
+if args.tfidf:
+    print("Applying tf-idf to raw counts.")
+    tfidf_transformer = TfidfTransformer()
+    word_scores = tfidf_transformer.fit_transform(word_scores)
+
+# if svd is specified, use svd to reduce dimensions to specified amount
+if args.svddims:
+    print("Truncating matrix to {} dimensions via singular value decomposition.".format(args.svddims))
+    svd = TruncatedSVD(n_components=args.svddims)
+    word_scores = svd.fit_transform(word_scores)
+
+try:
+    # this is needed to convert the sparse matrix to a dataframe; it needs to be convered to a numpy array first
+    word_scores = word_scores.toarray()
+except:
+    # it is already numpy array; it was converted to one through SVD
+    pass
+
+try:
+    # create a dataframe with the same columns as df_word_counts
+    word_scores = pd.DataFrame(word_scores, columns=df_word_counts.columns)
+except:
+    # if SVD was applied, then the column names from df_word_counts can't be applied since the dimensionality has been changed
+    word_scores = pd.DataFrame(word_scores)
+
+# create a multi-index using filepaths and filenames
+new_index = pd.MultiIndex.from_tuples(list(zip(filepaths, filenames)))
+word_scores.index = new_index
 
 print("Writing matrix to {}.".format(args.outputfile))
+
+word_scores.to_csv(args.outputfile)
+
--- simdoc.py
+++ simdoc.py
@@ -4,10 +4,32 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-# simdoc.py -- Don't forget to put a reasonable amount code comments
-# in so that we better understand what you're doing when we grade!
+# this function receives one or two dataframes, where each dataframe contains the vectors of all articles of one topic
+# if one dataframe is specified, the average vector similarity of each article in the topic dataframe is computed with vector of each article in the same topic
+# if two dataframes are specified, the average vector similarity of each article in first topic dataframe is computed with vector of each article in the second topic dataframe
+def compute_avg_similarity(topic_1_articles, topic_2_articles = None):
+    # this means that we compute average similarity of each article in topic 1 over topic 1, averaged out
+    if topic_2_articles is None:
+        comparison_topic_articles = topic_1_articles
+    # this means that we compute average similarity of each article in topic 1 over topic 2, averaged out
+    else:
+        comparison_topic_articles = topic_2_articles
+        
+    avg_cosine_sim_topic = []
+    similarities = cosine_similarity(topic_1_articles, comparison_topic_articles)
+    avg_cosine_sim_topic = np.mean(similarities)
+    # for index, row in topic_1_articles.iterrows():
+    #     avg_cosine_sim_doc = []
+    #     for sub_index, sub_row in comparison_topic_articles.iterrows():
+    #         cosine_sim = cosine_similarity([row], [sub_row])
+    #         avg_cosine_sim_doc.append(cosine_sim[0][0])
+    #     avg_cosine_sim_doc = sum(avg_cosine_sim_doc)/len(avg_cosine_sim_doc)
+    #     avg_cosine_sim_topic.append(avg_cosine_sim_doc)
+    
+    # avg_cosine_sim_topic = sum(avg_cosine_sim_topic)/len(avg_cosine_sim_topic)
+    return avg_cosine_sim_topic
 
-# add whatever additional imports you may need here
+
 
 parser = argparse.ArgumentParser(description="Compute some similarity statistics.")
 parser.add_argument("vectorfile", type=str,
@@ -15,4 +37,37 @@ parser.add_argument("vectorfile", type=str,
 
 args = parser.parse_args()
 
+if not args.vectorfile.lower().endswith("csv"):
+	print("Please specify an input file of .csv format!")
+	exit(1)
+
 print("Reading matrix from {}.".format(args.vectorfile))
+# read csv file specified in command line arguments
+df = pd.read_csv(args.vectorfile, index_col=[0,1], skipinitialspace=True)
+
+df_levels = list(df.groupby(level=0))
+if len(df_levels) < 1:
+	print("Please specify a file containing multi-indexed data of two different topics")
+	exit(1)
+
+print("Computing similarities...")
+# get the topic names
+topic_1 = df_levels[0][0].split("/")[-1]
+topic_2 = df_levels[1][0].split("/")[-1]
+# get the topic dataframes
+topic_1_articles = df_levels[0][1]
+topic_2_articles = df_levels[1][1]
+
+# compute average similarity over all articles in topic 1, averaged out
+avg_similarity_crude_crude = compute_avg_similarity(topic_1_articles)
+# compute average similarity over all articles in topic 1, averaged out
+avg_similarity_grain_grain = compute_avg_similarity(topic_2_articles)
+# compute average similarity over all articles in topic 1 with all articles in topic 2, averaged out
+avg_similarity_crude_grain = compute_avg_similarity(topic_1_articles, topic_2_articles)
+# compute average similarity over all articles in topic 2 with all articles in topic 1, averaged out
+avg_similarity_grain_crude = compute_avg_similarity(topic_2_articles, topic_1_articles)
+
+print("Average similarity - %s %s: %f" % (topic_1, topic_1, avg_similarity_crude_crude))
+print("Average similarity - %s %s: %f" % (topic_1, topic_2, avg_similarity_crude_grain))
+print("Average similarity - %s %s: %f" % (topic_2, topic_1, avg_similarity_grain_crude))
+print("Average similarity - %s %s: %f" % (topic_2, topic_2, avg_similarity_grain_grain))
